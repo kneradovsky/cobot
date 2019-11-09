c59084cf7b5b0d3eb5ae565ced3c1643a78fe807
kneradovsky <kneradovsky@gmail.com>
2019-11-09 15:39:35 +0300

    auth api url fixed

--- admin/src/app/auth.service.ts
+++ admin/src/app/auth.service.ts
@@ -2,19 +2,19 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {map} from 'rxjs/operators';
+import { ConfigService } from './config.service';
 
 const tokenVar = 'access_token';
-const apiServer = 'http://localhost:8080';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private cfg:ConfigService, private http: HttpClient) { }
 
   login(username: string, password: string): Observable<boolean> {
-    return this.http.post<{user: string, token: string}>(apiServer + '/api/auth',{user:{username: username,password:password}})
+    return this.http.post<{user: string, token: string}>(this.cfg.apiUrl + '/auth',{user:{username: username,password:password}})
     .pipe(
       map(res => {
         localStorage.setItem(tokenVar, res.token);
